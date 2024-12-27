0730ceeab10a817fc9cdd605325b76c763c289d3
amaliaard <amaliaardiyani29@gmail.com>
2024-12-27 19:15:27 +0700

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Product::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'numeric',
+        ]);
+
+        return Product::create($request->all());
+    }
+
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    public function update(Request $request, Product $product)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'price' => 'numeric',
+        ]);
+
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(null, 204);
+    }
+}
